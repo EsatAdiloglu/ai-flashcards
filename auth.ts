b75a3b8e6c4a2c8ea907055f69ad9407973523fe
Nathaniel Escaro <butterman423@gmail.com>
2024-08-12 20:19:06 -0400

    Modified jwt content

--- auth.ts
+++ auth.ts
@@ -8,23 +8,16 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: FirestoreAdapter(firestore),
   session: { strategy: 'jwt' },
   callbacks: {
-    // Thank god for this
-    // https://github.com/nextauthjs/next-auth/issues/7913
-
     jwt: async ({ token, account }) => {
-      if (account && account.access_token) {
-          token.accessToken = account.access_token; // <-- adding the access_token here
-          token.provider = account.provider;
-          token.providerId = account.providerAccountId;
+      if(account && account.userId) {
+        token.id = account.userId;
       }
-      return token
+      return token;
     },
     session: async ({ session, token }) => {
       // If we want to make the accessToken available in components, then we have to explicitly forward it here.
       return { ...session, 
-        access_token: token.accessToken, 
-        provider: token.provider,
-        providerId: token.providerId
+        id: token.id
       }
   },
 
