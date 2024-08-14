c2604cdeb41b7cb301005ee3c3388413cc28c616
Nathaniel Escaro <butterman423@gmail.com>
2024-08-14 19:57:27 -0400

    Split Flashcard page

---
+++ app/cards/[id]/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import type { CardContent } from '@/components/Card';
+
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+
+import PromptField from '@/components/PromptField';
+import CardGrid from '@/components/CardGrid';
+
+import { useState } from 'react';
+
+export default function CardPage() {
+  const [cards, setCards] = useState<CardContent[]>([]);
+
+  async function handleSubmit(prompt: string) {
+    // TODO: Get flashcards of the provided prompt
+  }
+
+  return (
+    <Container>
+      <Typography variant='h1' textAlign='center'>
+        Headstarter Flashcards
+      </Typography>
+      <Divider/>
+
+      <PromptField onSubmit={handleSubmit}/>
+      <Divider/>
+
+      <Box my={3}>
+        {
+          cards.length > 0
+          ? <CardGrid cards={cards}/>
+          : <Typography textAlign='center'>No cards to show.</Typography>
+        }
+      </Box>
+      
+    </Container>
+  );
+}
