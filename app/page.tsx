f84f0f101736520f3bc396a0eaddd8f4f736f2a3
Nathaniel Escaro <butterman423@gmail.com>
2024-08-14 16:17:15 -0400

    Moved card page to /cards route

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,5 @@
-'use client'
-
-import type { CardContent } from '@/components/Card';
-
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-
-import PromptField from '@/components/PromptField';
-import CardGrid from '@/components/CardGrid';
-
-import { useState } from 'react';
-
 export default function Home() {
-  const [cards, setCards] = useState<CardContent[]>([]);
-
-  async function handleSubmit(prompt: string) {
-    // TODO: Get flashcards of the provided prompt
-  }
-
-  return (
-    <main>
-      <Container>
-        <Typography variant='h1' textAlign='center'>
-          Headstarter Flashcards
-        </Typography>
-        <Divider/>
-
-        <PromptField onSubmit={handleSubmit}/>
-        <Divider/>
-
-        <Box my={3}>
-          {
-            cards.length > 0
-            ? <CardGrid cards={cards}/>
-            : <Typography textAlign='center'>No cards to show.</Typography>
-          }
-        </Box>
-        
-      </Container>
-    </main>
-  );
+    return (
+        <h1>Hello World</h1>
+    );
 }
