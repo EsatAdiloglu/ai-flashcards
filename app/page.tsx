c2c6b01934b1d98545d93d8da0434f81205799a0
Nathaniel Escaro <butterman423@gmail.com>
2024-08-16 14:54:12 -0400

    Moved Head metadata to root layout - Removed unneeded imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 import getStripe from '@/utils/get-stripe'
 import {SignedIn, SignedOut, UserButton} from '@clerk/nextjs'
 import {AppBar, Box, Button, Container, Toolbar, Typography} from "@mui/material"
-import Head from 'next/head'
 
 export default function Home() {
     return (
         <Container maxWidth = "lg">
-
-            <Head>
-                <title>Flashcard SaaS</title>
-                <meta name = "description" content = "Create flashcard"/>
-            </Head>
-
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant = "h6" style = {{flexGrow: 1}}>Flashcard SaaS</Typography>
