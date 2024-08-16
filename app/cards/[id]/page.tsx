75af417d710f676db28461507b3a3af51ead7fdd
EsatAdiloglu <esatadiloglu16@gmail.com>
2024-08-16 01:56:05 -0400

    added firebase to cards/[id] but need to add feature of not adding the same flashcard

--- app/cards/[id]/page.tsx
+++ app/cards/[id]/page.tsx
@@ -11,11 +11,32 @@ import PromptField from '@/components/PromptField';
 import List from '@/components/List';
 import Card from '@/components/Card';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import { db } from "@/firebase"
+import { collection, doc, getDoc, getDocs, query, writeBatch } from 'firebase/firestore';
+import { Button } from '@mui/material';
 
+const test = {flashcards: [{front:"what is 1+1?",back:"11"},{front:"how many bits is in a byte?",back:"8 bits"},{front:"Dağ",back:"Mountain"},
+  {front:"what blade does Esat fence?",back:"Epee"},{front:"How many Ls does Sai take?",back:"Way to many"},{front:"What is a child process's id",back:"0"},
+  {front:"What should Nathaniel do?",back:"Go to the gym"},{front:"What anime should Ryan watch?",back:"toradora"},{front:"What year did America declare independence",back:"1776"},
+  {front:"What mascot does Stevens have",back:"Attila duck"}]}
+
+  const test2 = {flashcards: [{front:"a^2 + b^2 =?",back:"c^2"},{front:"gojo?",back:"satoru"},{front:"which came first the chicken or the egg?",back:"egg"},{front:"what should i be doing",back:"sleep"}]}
 
 export default function CardPage() {
   const [cards, setCards] = useState<CardContent[]>([]);
+  const [setName, setSetName] = useState("")
+  const param = useParams<{id: string}>()
+
+
+  useEffect(() => {
+    setSetName(param.id)
+  },[])
+
+  useEffect(() => {
+    updateFlashCards()
+  },[setName])
 
   async function handleSubmit(prompt: string) {
     // TODO: Get flashcards of the provided prompt
@@ -25,11 +46,69 @@ export default function CardPage() {
     return (<Card key={idx} front={front} back={back}/>)
   });
 
+  const updateFlashCards = async () => {
+    try{
+      if(setName){
+        const userDocRef = doc(collection(db,"users"),"test")
+        const colRef = collection(userDocRef, setName)
+        const snapshot = query(colRef)
+        const docs = await getDocs(snapshot)
+        const current_flashcards: CardContent[] = []
+        docs.forEach(flashcard => {
+          //idk why, it just works
+          current_flashcards.push({front: flashcard.data().back, back: flashcard.data().front})
+        })
+        setCards(current_flashcards)
+      }
+    }
+    catch (error){
+      console.error(error)
+    }
+  }
+
+  const addFlashCards = async () => {
+    try{
+      const batch = writeBatch(db)
+      const userDocRef = doc(collection(db,"users"),"test")
+      const colRef = collection(userDocRef, setName)
+      test.flashcards.forEach(flashcard  => {
+        const cardDocRef = doc(colRef)
+        batch.set(cardDocRef, flashcard)
+      })
+
+      await batch.commit()
+      await updateFlashCards()
+    }
+    catch(error){
+      console.error(error)
+    }
+  }
+
+    const addFlashCards2 = async () => {
+      try{
+        const batch = writeBatch(db)
+        const userDocRef = doc(collection(db,"users"),"test")
+        const colRef = collection(userDocRef, setName)
+        test2.flashcards.forEach((flashcard: CardContent) => {
+          const cardDocRef = doc(colRef)
+          batch.set(cardDocRef, flashcard)
+        })
+  
+        await batch.commit()
+  
+      }
+      catch(error){
+        console.error(error)
+      }
+    }
+
   return (
     <Container>
       <Typography variant='h1' textAlign='center'>
         Headstarter Flashcards
       </Typography>
+      <Button onClick={addFlashCards}>Click</Button>
+      <Button onClick={addFlashCards2}>Click2</Button>
       <Divider/>
 
       <PromptField onSubmit={handleSubmit}/>
@@ -39,7 +118,11 @@ export default function CardPage() {
         {
           cards.length > 0
           ? <List>{ cardEls }</List>
-          : <Typography textAlign='center'>No cards to show.</Typography>
+          : 
+          <Box>
+          <Typography textAlign='center'>No cards to show.</Typography>
+          
+          </Box>
         }
       </Box>
       
