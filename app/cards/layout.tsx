b4fd49080609a68cbee2c3ca6bb1e9e2bad26cca
Nathaniel Escaro <butterman423@gmail.com>
2024-08-15 20:56:06 -0400

    Moved nav bar to affect all pages under /cards

--- app/cards/layout.tsx
+++ app/cards/layout.tsx
@@ -1,3 +1,7 @@
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
 export default function CardLayout({
     children,
   }: Readonly<{
@@ -5,6 +9,10 @@ export default function CardLayout({
   }>) {
     return (
       <>
+        <AppBar position="static" sx={{display:"flex", flexDirection:"row", height:"10%"}}>
+            <Typography variant="h2" sx={{flexGrow: 1, ml: "20px"}}>Test</Typography>
+            <Button color="inherit" sx={{width:"100px"}}>Sign Out</Button>
+        </AppBar>
         <main>{ children }</main>
       </>
     );
